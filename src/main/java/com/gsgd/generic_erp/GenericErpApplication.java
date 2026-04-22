8901410fc02adf640e4a2c8b84551a670e766d93
genesisyoung <genesis.young.personal@gmail.com>
2026-04-22 01:53:48 -0400

    Initialization

---
+++ src/main/java/com/gsgd/generic_erp/GenericErpApplication.java
@@ -0,0 +1,13 @@
+package com.gsgd.generic_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsgd/generic_erp/GenericErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsgd.generic_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
