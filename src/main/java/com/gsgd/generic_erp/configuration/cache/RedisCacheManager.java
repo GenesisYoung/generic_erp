c47939a741335bee7e6b219579c139013980a647
genesisyoung <genesis.young.personal@gmail.com>
2026-08-05 22:17:04 -0400

    cache updates

--- src/main/java/com/gsgd/generic_erp/configuration/cache/RedisCacheManager.java
+++ src/main/java/com/gsgd/generic_erp/configuration/cache/RedisCacheManager.java
@@ -1,11 +1,13 @@
 package com.gsgd.generic_erp.configuration.cache;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableCaching
 public class RedisCacheManager {
     @Bean
     public CacheManager cacheManager() {
